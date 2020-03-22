0d9f395855e4d6e7bf1d010a373af279aba8286e
tomoyaueno29 <u.t01280729@gmail.com>
2020-03-23 01:17:47 +0900

    Update creators_controller.rb

--- app/controllers/creators_controller.rb
+++ app/controllers/creators_controller.rb
@@ -1,2 +1,24 @@
 class CreatorsController < ApplicationController
+  def category
+    @creator = Creator.new
+  end
+
+  def restriction
+    session[:type] = creator_params[:type]
+    # binding.pry
+    @creator = Creator.new
+  end
+
+  def create
+    session[:restriction] = creator_params[:restriction]
+    @creator = Creator.new(
+      type: session[:type],
+      restriction: session[:restriction]
+    )
+    if @creator.save
+      session[:id] = @creator.id
+    else
+      render '/creators/step1'
+    end
+  end
 end
