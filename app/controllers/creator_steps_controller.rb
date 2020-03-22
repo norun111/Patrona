a91ed56da2c530fb6bc44a1333c12e037460d3c6
tomoyaueno29 <u.t01280729@gmail.com>
2020-03-23 04:26:27 +0900

    Update creator_steps_controller.rb

--- app/controllers/creator_steps_controller.rb
+++ app/controllers/creator_steps_controller.rb
@@ -3,6 +3,24 @@ class CreatorStepsController < ApplicationController
   steps :category, :restriction
 
   def show
+    @creator = current_creator
     render_wizard
   end
+
+  def update
+    @creator = current_creator
+    @creator.attributes = creator_params
+    render_wizard @creator
+  end
+
+  private
+  def current_creator
+    Creator.find_by(id: session[:creator_id])
+  end
+
+  def creator_params
+    params.require(:creator).permit(:podcasts, :videos, :music, :no, :prohibited)
+  end
+
+  
 end
