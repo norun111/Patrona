c6c3fee4b9225497ab878d9de1db6684cffe42e7
tomoyaueno29 <u.t01280729@gmail.com>
2020-03-23 20:18:33 +0900

    Create goal.js

---
+++ app/assets/javascripts/goal.js
@@ -0,0 +1,16 @@
+$(document).ready(function() { 
+  $(".user-goal__bottom").not('.slick-initialized').slick({
+    slidesToShow:1,
+    slidesToScroll:1,
+    accessibility:false,
+    arrows:false
+  });
+  $('.fa-chevron-right').on('click',function(){
+    $('.user-goal__bottom').slick('slickNext');
+  });
+
+  $('.fa-chevron-left').on('click',function(){
+    $('.user-goal__bottom').slick('slickPrev');
+  });
+  
+});
