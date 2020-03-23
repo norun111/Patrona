5af5a9f51c5b08dee28686eab308dcc09539b859
tomoyaueno29 <u.t01280729@gmail.com>
2020-03-23 16:33:32 +0900

    Create checkbox.js

---
+++ app/assets/javascripts/checkbox.js
@@ -0,0 +1,11 @@
+$(function() {
+  $('#category-submit-btn').attr('disabled', 'disabled');
+
+  $('#creator_podcasts, #creator_videos, #creator_music').click(function() {
+    if ( $(this).prop('checked') == false ) {
+      $('#category-submit-btn').attr('disabled', 'disabled');
+    } else {
+      $('#category-submit-btn').removeAttr('disabled');
+    }
+  });
+});
