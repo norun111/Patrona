b50fed27bf66bdc4d8b7485d51ec8a080757819f
tomoyaueno29 <u.t01280729@gmail.com>
2020-03-23 21:51:16 +0900

    Update contents_controller.rb

--- app/controllers/contents_controller.rb
+++ app/controllers/contents_controller.rb
@@ -1,2 +1,14 @@
 class ContentsController < ApplicationController
+
+  def update
+    respond_to do |format|
+      if @content.update(content_params) && @content.video.recreate_versions!
+        format.html { redirect_to @content, notice: 'Article was successfully updated.' }
+        format.json { head :no_content }
+      else
+        format.html { render action: 'edit' }
+        format.json { render json: @content.errors, status: :unprocessable_entity }
+      end
+    end
+  end
 end
