2120220d41cdda2cce5958ccaed5b6b91419cb76
tomoyaueno29 <u.t01280729@gmail.com>
2020-03-23 17:24:55 +0900

    Update 20200322154521_create_creators.rb

--- db/migrate/20200322154521_create_creators.rb
+++ db/migrate/20200322154521_create_creators.rb
@@ -1,10 +1,13 @@
 class CreateCreators < ActiveRecord::Migration[5.2]
   def change
     create_table :creators do |t|
+      t.string :creator_name        ,default: ""
+      t.string :creator_email       ,default: ""
       t.boolean :podcasts           ,default: false
       t.boolean :videos             ,default: false
       t.boolean :music              ,default: false
       t.integer :restriction, default: 0
+
 
       t.references :user, foreign_key: true
       t.timestamps
