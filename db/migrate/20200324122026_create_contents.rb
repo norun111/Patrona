c5904d048a4504759bdb3215b7608b7678a931ae
tomoyaueno29 <u.t01280729@gmail.com>
2020-03-24 16:37:44 +0900

    Create 20200324122026_create_contents.rb

---
+++ db/migrate/20200324122026_create_contents.rb
@@ -0,0 +1,8 @@
+class CreateContents < ActiveRecord::Migration[5.2]
+  def change
+    create_table :contents do |t|
+      t.references :video, foreign_key: true
+      t.timestamps
+    end
+  end
+end
