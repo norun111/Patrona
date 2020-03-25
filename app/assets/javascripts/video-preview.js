17433c362f72de2ca3bfc366f931d8c66a5531e0
tomoyaueno29 <u.t01280729@gmail.com>
2020-03-25 19:36:13 +0900

    Update video-preview.js

--- app/assets/javascripts/video-preview.js
+++ app/assets/javascripts/video-preview.js
@@ -1,28 +1,30 @@
-// $(document).on('turbolinks:load', function() {
-//   $videoField=$('#video-label')
+$(document).on('turbolinks:load', function() {
+  $videoField=$('#video-label')
 
-//   $($videoField).on('change', $videoField, function(e){
+  $($videoField).on('change', $videoField, function(e){
 
-//     file = e.target.files[0]
-//     reader = new FileReader(),
-//     $preview = $(".video-choice");
+    file = e.target.files[0]
+    reader = new FileReader(),
+    $preview = $(".aaa");
 
-//     reader.onload = (function(file){
-//       return function(e) {
-//         $preview.empty();
-//         $preview.append($('<video>').attr({
-//           src: e.target.result,
-//           width: "100%",
-//           height: "220px",
-//           class: "preview-video",
-//           autoplay: "autoplay",
-//           loop: "loop",
-//           playsinline: "true",
-//           title: file.name
-//         }));
-//       };
-//     })(file);
-//     reader.readAsDataURL(file);
-//   });
-// });
+    reader.onload = (function(file){
+      return function(e) {
+        $preview.empty();
+        $preview.append($('<video>').attr({
+          src: e.target.result,
+          width: "100%",
+          height: "220px",
+          class: "preview-video",
+          autoplay: "autoplay",
+          loop: "loop",
+          playsinline: "true",
+          title: file.name
+        }));
+      };
+    })(file);
+    console.log("aa");
+    reader.readAsDataURL(file);
+    
+  });
+});
 
