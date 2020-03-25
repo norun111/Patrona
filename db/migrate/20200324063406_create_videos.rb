c1e17db9ee81f5f84486d2c8934d97562fd1e856
tomoyaueno29 <u.t01280729@gmail.com>
2020-03-25 17:57:08 +0900

    Update 20200324063406_create_videos.rb

--- db/migrate/20200324063406_create_videos.rb
+++ db/migrate/20200324063406_create_videos.rb
@@ -1,7 +1,14 @@
 class CreateVideos < ActiveRecord::Migration[5.2]
   def change
     create_table :videos do |t|
-      t.string     :video
+      t.string     :video, null: false
+      t.string     :video_url, null: false
+      t.string     :image, null: false
+      t.string     :title, null: false
+      t.text       :description, null: false
+      
+      
+
       t.timestamps
     end
   end
