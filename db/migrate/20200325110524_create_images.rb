be6629a1d52e55a164afa331ff5bf3a3a5f6121a
tomoyaueno29 <u.t01280729@gmail.com>
2020-03-25 21:45:56 +0900

    Update 20200325110524_create_images.rb

--- db/migrate/20200325110524_create_images.rb
+++ db/migrate/20200325110524_create_images.rb
@@ -1,8 +1,9 @@
 class CreateImages < ActiveRecord::Migration[5.2]
   def change
     create_table :images do |t|
-      t.string :image, null: false
-      t.string :title, null: false
+      t.string :image, null: false, default: ""
+      t.string :title, null: false, default: ""
+      t.text :description, null: false
       t.timestamps
     end
   end
