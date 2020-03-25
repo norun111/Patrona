3f0ff4d2b4c2ca440360f51cfd6dbb2dbd65c119
tomoyaueno29 <u.t01280729@gmail.com>
2020-03-25 17:57:25 +0900

    Update videos_controller.rb

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -1,8 +1,35 @@
 class VideosController < ApplicationController
+  before_action :set_video, only: [:show]
+
   def new
     @video = Video.new
   end
 
   def create
+    @video = Video.new(video_params)
+   
+    
+    respond_to do |format|
+      if @video.save
+        format.html { redirect_to video_url(@video.id), notice: 'Article was successfully created.' }
+        format.json { render action: 'show', status: :created, location: @video }
+      else
+        format.html { render action: 'new' }
+        format.json { render json: @video.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def show
+  end
+
+  private 
+  def set_video
+    @video = Video.find(params[:id])
+  end
+
+
+  def video_params
+    params.require(:video).permit(:video, :video_url, :title, :image, :description)
   end
 end
