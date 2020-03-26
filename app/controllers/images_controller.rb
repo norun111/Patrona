58f81296f9527c0a96923344083d8e298ad2c6df
tomoyaueno29 <u.t01280729@gmail.com>
2020-03-26 17:18:46 +0900

    Update images_controller.rb

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -1,11 +1,35 @@
 class ImagesController < ApplicationController
-  def new
-    @image = Content.new
+  include Wicked::Wizard
+  steps :image_step
+  
+  def show
+    @content = current_content
+    render_wizard
+  end
+
+  def update
+    @content = current_content
+    @content.attributes = content_params
+    # @creator = current_creator
+    render_wizard @content
+  end
+
+  private
+  def content_params
+    params.require(:content).permit(
+      :image,
+      :image_title,
+      :image_description
+    )
+  end
+
+  # def redirect_to_finish_wizard(options = nil, params = {})
+  #   redirect_to creator_path(@creator.id)
+  # end
+
+  def current_content
+    Content.find_by(id: session[:creator_id])
   end
 
 
-  # private
-  # def image_params
-  #   params.require(:image).permit(:image,:title,:description)
-  # end
 end
