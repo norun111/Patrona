e18f6d9f2ff708a3f1db1014776fd16807eb64ab
tomoyaueno29 <u.t01280729@gmail.com>
2020-03-26 17:18:48 +0900

    Update contents_controller.rb

--- app/controllers/contents_controller.rb
+++ app/controllers/contents_controller.rb
@@ -2,13 +2,37 @@ class ContentsController < ApplicationController
   def post
   end
 
+  def new
+    @content = Content.new
+  end
+
   def create
-    @image = Content.new(content_params)
-    @image.save
+    @content = Content.new(content_params)
+    @content["creator_id"] = current_creator.id
+    
+    if params[:image_send]
+      if @content.save
+        session[:id] = @content.id
+        redirect_to images_path
+      else
+        render :new
+      end
+    else
+      if @content.save
+        session[:id] = @content.id
+        redirect_to videos_path
+      else
+        render :new
+      end
+    end
   end
 
   private
   def content_params
-    params.require(:content).permit(:image,:image_title,:image_description,:video,:video_url,:thumbnail,:video_title,:video_description,:creator_id)
+    params.require(:content).permit(:creator_id)
+  end
+
+  def current_creator
+    Creator.find_by(id: session[:creator_id])
   end
 end
