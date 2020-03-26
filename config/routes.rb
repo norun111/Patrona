b4ed7cdd6eb2adfb33bde27a50770fe6f5c255fa
tomoyaueno29 <u.t01280729@gmail.com>
2020-03-26 20:01:48 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,8 @@ Rails.application.routes.draw do
   root "tops#home"
   get  '/demo',    to: 'creators#demo'
   get  '/post',    to: 'contents#post'
+  get  '/video_new', to: 'contents#video_new'
+  post '/video_create', to: 'contents#video_create'
 
   resources :users, only:[:show]
   resources :creators, only:[:new,:create, :show]
