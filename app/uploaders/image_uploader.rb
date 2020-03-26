86c66188252a67f757c8860ca7b9ff956648e1e4
tomoyaueno29 <u.t01280729@gmail.com>
2020-03-27 07:35:18 +0900

    Update image_uploader.rb

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -1,7 +1,7 @@
 class ImageUploader < CarrierWave::Uploader::Base
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
-  # include CarrierWave::MiniMagick
+  include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
   storage :file
@@ -20,7 +20,9 @@ class ImageUploader < CarrierWave::Uploader::Base
   #
   #   "/images/fallback/" + [version_name, "default.png"].compact.join('_')
   # end
-
+  # version :midium do
+  #   process resize_to_fill: [480, 480]
+  # end
   # Process files as they are uploaded:
   # process scale: [200, 300]
   #
