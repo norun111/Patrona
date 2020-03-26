eecc7351ee8d0a00a9bf2082f6c7c58f31f97f34
tomoyaueno29 <u.t01280729@gmail.com>
2020-03-27 04:39:46 +0900

    Update content.rb

--- app/models/content.rb
+++ app/models/content.rb
@@ -3,6 +3,7 @@ class Content < ApplicationRecord
   
   mount_uploader :image, ImageUploader
   mount_uploader :thumbnail, ThumbnailUploader
+  mount_uploader :audio_image, ImageUploader
   mount_uploader :video, VideoUploader
   mount_uploader :audio, AudioFileUploader
 end
