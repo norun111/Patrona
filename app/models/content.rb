61a7174143cfedab598fa15dc24a5606a7f87f1e
tomoyaueno29 <u.t01280729@gmail.com>
2020-03-26 17:18:42 +0900

    Update content.rb

--- app/models/content.rb
+++ app/models/content.rb
@@ -1,6 +1,7 @@
 class Content < ApplicationRecord
   belongs_to :creator
-  
+  has_one :image
+  accepts_nested_attributes_for :image
   mount_uploader :image, ImageUploader
   mount_uploader :thumbnail, ThumbnailUploader
   mount_uploader :video, VideoUploader
