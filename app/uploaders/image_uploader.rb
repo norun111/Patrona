2f9bad7017232df6ccef4097cca6a07939114777
tomoyaueno29 <u.t01280729@gmail.com>
2020-03-27 08:59:05 +0900

    Update image_uploader.rb

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -21,7 +21,7 @@ class ImageUploader < CarrierWave::Uploader::Base
   #   "/images/fallback/" + [version_name, "default.png"].compact.join('_')
   # end
   
-  process resize_to_fit: [478, 700]
+  process resize_to_fit: [578, 700]
  
   # Process files as they are uploaded:
   # process scale: [200, 300]
