02698d4e889c7e0d56716e3a9b168549ad80aaac
tomoyaueno29 <u.t01280729@gmail.com>
2020-03-27 21:06:14 +0900

    Update tops_controller.rb

--- app/controllers/tops_controller.rb
+++ app/controllers/tops_controller.rb
@@ -1,4 +1,10 @@
 class TopsController < ApplicationController
   def home
+    @creator = current_creator
+  end
+  
+  private
+  def current_creator
+    Creator.find_by(id: session[:creator_id])
   end
 end
