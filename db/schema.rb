ff8178f2f4b7b25f13672958fb79424dc5ebbdfa
tomoyaueno29 <u.t01280729@gmail.com>
2020-03-29 18:28:37 +0900

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_25_122026) do
+ActiveRecord::Schema.define(version: 2020_03_29_092113) do
+
+  create_table "comments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.text "body", null: false
+    t.bigint "user_id", null: false
+    t.bigint "creator_id", null: false
+    t.bigint "content_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["content_id"], name: "index_comments_on_content_id"
+    t.index ["creator_id"], name: "index_comments_on_creator_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
+  end
 
   create_table "contents", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "image", default: ""
@@ -58,6 +70,9 @@ ActiveRecord::Schema.define(version: 2020_03_25_122026) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "comments", "contents"
+  add_foreign_key "comments", "creators"
+  add_foreign_key "comments", "users"
   add_foreign_key "contents", "creators"
   add_foreign_key "creators", "users"
 end
