08938a262dce04e4110ecc2e13989f3990f488e7
tomoyaueno29 <u.t01280729@gmail.com>
2020-03-29 16:43:31 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -14,7 +14,7 @@ Rails.application.routes.draw do
   post '/audio_create', to: 'contents#audio_create'
 
   resources :users, only:[:show]
-  resources :creators, only:[:new,:create, :show]
+  resources :creators, only:[:index, :new, :create, :show]
   resources :creator_steps
   resources :contents
   
