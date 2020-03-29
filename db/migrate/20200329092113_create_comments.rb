ffef66ffb592e13e2c41d0bf689601e4506630b2
tomoyaueno29 <u.t01280729@gmail.com>
2020-03-29 18:28:30 +0900

    Update 20200329092113_create_comments.rb

--- db/migrate/20200329092113_create_comments.rb
+++ db/migrate/20200329092113_create_comments.rb
@@ -1,10 +1,10 @@
 class CreateComments < ActiveRecord::Migration[5.2]
   def change
     create_table :comments do |t|
-      t.text :body
-      t.references :user, foreign_key: true
-      t.references :creator, foreign_key: true
-      t.references :content, foreign_key: true
+      t.text :body,                           null: false
+      t.references :user, foreign_key: true,  null: false
+      t.references :creator, foreign_key: true, null: false
+      t.references :content, foreign_key: true, null: false
 
       t.timestamps
     end
