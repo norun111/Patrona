22e70e2e29b93e8a38e67840b9c4a07e4e885407
tomoyaueno29 <u.t01280729@gmail.com>
2020-03-29 18:28:34 +0900

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_one :creator
+  has_many :contents, dependent: :destroy
+  has_many :comments, dependent: :destroy
 
   mount_uploader :image, ImageUploader
 
