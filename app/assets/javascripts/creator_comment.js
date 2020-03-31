4b2e14ecde656245567b876df37ec6e334749ed7
tomoyaueno29 <u.t01280729@gmail.com>
2020-04-01 06:11:48 +0900

    Update creator_comment.js

--- app/assets/javascripts/creator_comment.js
+++ app/assets/javascripts/creator_comment.js
@@ -1,36 +1,36 @@
-$(document).on('turbolinks:load', function(){
-  $(function(){
-    function buildHTML(comment){
-      var html = `<li class="user-comment-index">
-                    <div class="user-comment-index__img">
-                      <img src="/assets/zenigame.jpg">
-                    </div>
-                    <div class="user-comment-index__body"> 
-                      <div class="user-comment-index__body--name">
-                        <a href=/creators/${comment.creator_id}>${comment.creator_name}</a>
-                      </div>
-                      <div class="user-comment-index__body--text">
-                        <p>${comment.body}</p>
-                      </div>
+// $(document).on('turbolinks:load', function(){
+//   $(function(){
+//     function buildHTML(comment){
+//       var html = `<li class="user-comment-index">
+//                     <div class="user-comment-index__img">
+//                       <img src="/assets/zenigame.jpg">
+//                     </div>
+//                     <div class="user-comment-index__body"> 
+//                       <div class="user-comment-index__body--name">
+//                         <a href=/creators/${comment.creator_id}>${comment.creator_name}</a>
+//                       </div>
+//                       <div class="user-comment-index__body--text">
+//                         <p>${comment.body}</p>
+//                       </div>
 
-                      <div class="user-comment-option-nest">
-                        <div class="user-comment-option">
-                          <div class="user-comment-option__reply">
-                            <i class="fas fa-reply"></i>
-                          </div>
-                          <div class="user-comment-option__like">
-                            <i class="far fa-heart"></i>
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-                  </li>`
-      return html;
-    }
-    $('.creator-post-comment__form').on('submit', function(e){
-      e.preventDefault();
-      var formData = new FormData(this);
-      var url = $(this).attr('action');
+//                       <div class="user-comment-option-nest">
+//                         <div class="user-comment-option">
+//                           <div class="user-comment-option__reply">
+//                             <i class="fas fa-reply"></i>
+//                           </div>
+//                           <div class="user-comment-option__like">
+//                             <i class="far fa-heart"></i>
+//                           </div>
+//                         </div>
+//                       </div>
+//                     </div>
+//                   </li>`
+//       return html;
+//     }
+//     $('.creator-post-comment__form').on('submit', function(e){
+//       e.preventDefault();
+//       var formData = new FormData(this);
+//       var url = $(this).attr('action');
       $.ajax({
         url: url,
         type: "POST",
