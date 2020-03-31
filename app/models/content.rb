beb1eb46c25a05b946f1323638e268e14ee6fdc0
tomoyaueno29 <u.t01280729@gmail.com>
2020-03-31 16:37:50 +0900

    Update content.rb

--- app/models/content.rb
+++ app/models/content.rb
@@ -1,5 +1,6 @@
 class Content < ApplicationRecord
   belongs_to :creator
+  has_many :comments, dependent: :destroy
   
   mount_uploader :image, ImageUploader
   mount_uploader :thumbnail, ThumbnailUploader
