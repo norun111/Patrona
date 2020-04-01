6cc17585b3a589296704f6b7c7ceb48cf3de9a9c
tomoyaueno29 <u.t01280729@gmail.com>
2020-04-01 22:42:10 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -16,6 +16,7 @@ Rails.application.routes.draw do
   post '/video_create', to: 'contents#video_create'
   get  '/audio_new', to: 'contents#audio_new'
   post '/audio_create', to: 'contents#audio_create'
+  get  '/tiers/top', to: 'tiers#top'
 
   resources :users, only:[:show]
   resources :creators, only:[:index, :new, :create, :show] do
