87d976c84fcb13c589cf1f95b4f2b790e4ab8360
tomoyaueno29 <u.t01280729@gmail.com>
2020-04-01 17:45:51 +0900

    Update creator_comment.js

--- app/assets/javascripts/creator_comment.js
+++ app/assets/javascripts/creator_comment.js
@@ -31,23 +31,23 @@
 //       e.preventDefault();
 //       var formData = new FormData(this);
 //       var url = $(this).attr('action');
-      $.ajax({
-        url: url,
-        type: "POST",
-        data: formData,
-        dataType: 'json',
-        processData: false,
-        contentType: false
-      })
-      .done(function(data){
-        var html = buildHTML(data);
-        $('.content-comment').append(html);
-        $('.creator-post-comment__form--body').val('');
-        $('submit').prop('disabled', false);
-      })
-      .fail(function(){
-        alert('error');
-      })
-    });
-  });
-});
+//       $.ajax({
+//         url: url,
+//         type: "POST",
+//         data: formData,
+//         dataType: 'json',
+//         processData: false,
+//         contentType: false
+//       })
+//       .done(function(data){
+//         var html = buildHTML(data);
+//         $('.content-comment').append(html);
+//         $('.creator-post-comment__form--body').val('');
+//         $('submit').prop('disabled', false);
+//       })
+//       .fail(function(){
+//         alert('error');
+//       })
+//     });
+//   });
+// });
