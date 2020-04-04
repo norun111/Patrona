8975c929b8e6258f3854178366b3f438c416768c
tomoyaueno <u.t01280729@gmail.com>
2020-04-04 21:28:11 +0900

    Update comments_controller.rb

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,28 +2,24 @@ class CommentsController < ApplicationController
   before_action :set_comment, only: [:destroy]
 
   def create
-    @comment = Comment.new(comment_params)
+    @content = Content.find(params[:content_id])
+    @comment = @content.comments.build(comment_params)
     @comment.creator_name = current_user.creator.creator_name
+    
     @comment.save
 
     gon.creator_comment = @comment.creator_name
 
     respond_to do |format|
       format.html{redirect_to content_path(params[:content_id])}
-      format.json{ render '/comment/create', handlers: 'jbuilder' }
+      format.json{ render '/comments/create', handlers: 'jbuilder' }
     end
-  end
-
-  def edit
-  end
-
-  def update
   end
 
   def destroy
     @comment = Comment.find(params[:id])
     if @comment.destroy
-      
+      redirect_to content_path(current_user.creator.id)
     end
   end
 
@@ -33,7 +29,7 @@ class CommentsController < ApplicationController
   end
 
   def comment_params
-    params.require(:comment).permit(:body, :creator_name).merge(user_id: current_user.id, creator_id: current_user.creator.id, content_id: params[:content_id])
+    params.require(:comment).permit(:body, :creator_name).merge(user_id: current_user.id, creator_id: current_user.creator.id, content_id: params[:content_id] )
   end
 
   def current_creator
