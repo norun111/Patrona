c5ca9ee5e4ed4fa148284fa5ea7d85f5b5637c17
tomoyaueno <u.t01280729@gmail.com>
2020-04-04 22:34:47 +0900

    Update enter.js

--- app/assets/javascripts/enter.js
+++ app/assets/javascripts/enter.js
@@ -1,17 +1,19 @@
-$(function() {
-  var $form = $(".creator-post-comment__form");
-  var $ta = $(".creator-post-comment__form--body");
+$(document).on('turbolinks:load', function(){
+  $(function() {
+    var $form = $(".creator-post-comment__form");
+    var $ta = $(".creator-post-comment__form--body");
 
-  $(document).on("keypress", ".creator-post-comment__form--body", function(e) {
-    if (e.keyCode == 13) { 
-      if (e.shiftKey) { 
+    $(document).on("keypress", ".creator-post-comment__form--body", function(e) {
+      if (e.keyCode == 13) { 
+        if (e.shiftKey) { 
+          $.noop();
+        } else if ($ta.val().replace(/\s/g, "").length > 0) {
+          e.preventDefault();
+          $form.submit();
+        }
+      } else {
         $.noop();
-      } else if ($ta.val().replace(/\s/g, "").length > 0) {
-        e.preventDefault();
-        $form.submit();
       }
-    } else {
-      $.noop();
-    }
+    });
   });
 });
