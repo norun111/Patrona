7e08abea486ffd669b5da8f7b81baba0442573c2
tomoyaueno <u.t01280729@gmail.com>
2020-04-04 19:11:07 +0900

    Update comment.rb

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,7 +1,7 @@
 class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :creator
-  belongs_to :content
+  belongs_to :content, dependent: :destroy
 
   validates :body, presence: true, length: { maximum: 65_535 }
 end
