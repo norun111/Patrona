c55ade076cce35ced1f9637e994fd6b74c98242a
tomoyaueno29 <u.t01280729@gmail.com>
2020-04-14 00:57:31 +0900

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,12 +2,14 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable, :omniauthable, omniauth_providers: [:stripe_connect]
   has_one :creator
   has_many :comments, dependent: :destroy
 
   mount_uploader :image, ImageUploader
-
   
+  def can_receive_payment?
+    uid && provider? && access_code? &&  publishable_key?
+  end
 end
 
