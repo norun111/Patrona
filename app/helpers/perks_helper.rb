40080e5926a86bb72b69750a60e92576e207343c
tomoyaueno29 <u.t01280729@gmail.com>
2020-04-13 23:04:31 +0900

    Update perks_helper.rb

--- app/helpers/perks_helper.rb
+++ app/helpers/perks_helper.rb
@@ -1,2 +1,18 @@
 module PerksHelper
+  def author_of(resource)
+    user_signed_in? && resource.creator_id == current_user.creator.id
+  end
+
+  def admin?
+    user_signed_in? && current_user.admin?
+  end
+
+  # def can_receive_payment?
+  #   uid && provider? && access_code? &&  publishable_key?
+  # end
+
+  # def purchased_perk(perk)
+  #   user_signed_in? && current_user.perk_subscriptions.include?("#{perk.title.parameterize}-perk_
+  #   #{perk.id}")
+  # end
 end
