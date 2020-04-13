0e33f316619b879d46ac4027f8c798aac73bef61
tomoyaueno29 <u.t01280729@gmail.com>
2020-04-13 23:03:53 +0900

    Update 20200406162605_create_perks.rb

--- db/migrate/20200406162605_create_perks.rb
+++ db/migrate/20200406162605_create_perks.rb
@@ -3,8 +3,8 @@ class CreatePerks < ActiveRecord::Migration[5.2]
     create_table :perks do |t|
       t.string :title, null:false
       t.decimal :amount, precision: 8, default: 0, null:false
-      t.text :description, null:false
-      t.string     :image
+      t.text :description
+      t.string :image
       t.references :creator, foreign_key: true, null:false
 
       t.timestamps
