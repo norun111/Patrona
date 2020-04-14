74ffaeae775a1173dcd2389afa967f0248196daa
tomoyaueno29 <u.t01280729@gmail.com>
2020-04-14 18:45:59 +0900

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable, :omniauthable, omniauth_providers: [:stripe_connect]
   has_one :creator
   has_many :comments, dependent: :destroy
+  has_many :perks, dependent: :destroy
 
   mount_uploader :image, ImageUploader
   
