6b91e55517b246659fbbf747328aeeb7abbc9c58
tomoyaueno29 <u.t01280729@gmail.com>
2020-04-15 22:53:21 +0900

    Update nav.js

--- app/assets/javascripts/nav.js
+++ app/assets/javascripts/nav.js
@@ -5,7 +5,7 @@ $(function() {
     var nav = $("#nav");
   $win.on('load scroll', function() {
     var value = $(this).scrollTop();
-    if ( value > 1200 ) {
+    if ( value > 800 ) {
       $cloneNav.addClass(showClass);
       nav.css('display','block');
     } else {
