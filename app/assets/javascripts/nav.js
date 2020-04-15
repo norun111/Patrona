960393a9535336c99748037eaba7a6eae5104b8c
tomoyaueno29 <u.t01280729@gmail.com>
2020-04-15 23:44:11 +0900

    Update nav.js

--- app/assets/javascripts/nav.js
+++ app/assets/javascripts/nav.js
@@ -1,16 +1,19 @@
-$(function() {
-  var $win = $(window),
-    $cloneNav = $('#nav').clone().addClass('clone-nav').appendTo('body'),
-    showClass = 'is-show';
-    var nav = $("#nav");
-  $win.on('load scroll', function() {
-    var value = $(this).scrollTop();
-    if ( value > 800 ) {
-      $cloneNav.addClass(showClass);
-      nav.css('display','block');
-    } else {
-      $cloneNav.removeClass(showClass);
-      nav.css('display','none');
-    }
+$(document).on('turbolinks:load', function(){
+
+  $(function() {
+    var $win = $(window),
+      $cloneNav = $('#nav').clone().addClass('clone-nav').appendTo('body'),
+      showClass = 'is-show';
+      var nav = $("#nav");
+    $win.on('load scroll', function() {
+      var value = $(this).scrollTop();
+      if ( value > 800 ) {
+        $cloneNav.addClass(showClass);
+        nav.css('display','block');
+      } else {
+        $cloneNav.removeClass(showClass);
+        nav.css('display','none');
+      }
+    });
   });
 });
