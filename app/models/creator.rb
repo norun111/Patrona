5f43c5a7b64d97655db7f7d296e6fff2fc2c300d
tomoyaueno29 <u.t01280729@gmail.com>
2020-04-19 16:53:42 +0900

    Update creator.rb

--- app/models/creator.rb
+++ app/models/creator.rb
@@ -1,6 +1,8 @@
 class Creator < ApplicationRecord
   enum restriction: {no:1, prohibited:2}
-  
+  enum dm_permission: {free: 1, patron_only: 2}
+  # validates :dm_permission, inclusion: {in: ["free", "patron_only"]}
+
   belongs_to :user
   has_many :contents, dependent: :destroy
   has_many :comments, dependent: :destroy
