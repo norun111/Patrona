21b0a187fab3150bf10d53c1269879c891af540b
tomoyaueno29 <u.t01280729@gmail.com>
2020-04-21 18:27:24 +0900

    Update creators_controller.rb

--- app/controllers/creators_controller.rb
+++ app/controllers/creators_controller.rb
@@ -40,24 +40,6 @@ class CreatorsController < ApplicationController
   end
 
   def show
-    @currentUserEntry=Entry.where(user_id: current_user.id)
-    @userEntry=Entry.where(user_id: @creator.user.id)
-    if @creator.user.id == current_user.id
-    else
-      @currentUserEntry.each do |cu|
-        @userEntry.each do |u|
-          if cu.room_id == u.room_id then
-            @isRoom = true
-            @roomId = cu.room_id
-          end
-        end
-      end
-      if @isRoom
-      else
-        @room = Room.new
-        @entry = Entry.new
-      end
-    end
   end
 
   def creator_perks
