3e8eba723e8d0b20276dd599d189e8369084e98e
tomoyaueno29 <u.t01280729@gmail.com>
2020-04-24 22:47:54 +0900

    Update creators_controller.rb

--- app/controllers/creators_controller.rb
+++ app/controllers/creators_controller.rb
@@ -40,7 +40,22 @@ class CreatorsController < ApplicationController
   end
 
   def show
-    
+    @rooms = @creator.rooms
+    unless current_user.creator
+      rooms = current_user.rooms
+      #自分が入ってるroomの相手のidを格納する
+      @creator_ids = []
+      rooms.each do |r|
+        @creator_ids << r.creator_id
+      end
+    else
+      rooms = current_user.creator.rooms
+      #自分が入ってるroomの相手のidを格納する
+      @user_ids = []
+      rooms.each do |r|
+        @user_ids << r.user_id
+      end
+    end
   end
 
   def creator_perks
