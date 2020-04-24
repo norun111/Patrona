9dc899ff65e39b4d7a3eac3ebf8fcff15b973a05
tomoyaueno29 <u.t01280729@gmail.com>
2020-04-24 22:21:21 +0900

    Create 20200424131700_create_messages.rb

---
+++ db/migrate/20200424131700_create_messages.rb
@@ -0,0 +1,13 @@
+class CreateMessages < ActiveRecord::Migration[5.2]
+  def change
+    create_table :messages do |t|
+      t.references :room, foreign_key: true
+      t.references :user, foreign_key: true
+      t.references :creator, foreign_key: true
+      t.boolean :is_user
+      t.text :content
+      
+      t.timestamps
+    end
+  end
+end
