8f643b352dcb5bf8e95cebd038ae3e9bd0df3190
tomoyaueno29 <u.t01280729@gmail.com>
2020-04-26 18:55:19 +0900

    Update room.js

--- app/assets/javascripts/channels/room.js
+++ app/assets/javascripts/channels/room.js
@@ -2,8 +2,8 @@ $(function(){
   return App.room = App.cable.subscriptions.create({
     channel: "RoomChannel",
     room_id: $('#messages').data('room-id'),
-    creator_id: $('#messages-creator').data('creator-id')
-    
+    creator_id: $('#messages-creator').data('creator-id'),
+    user_id: $('#messages-user').data('user-id')
   }, {
     connected: function() {
 
@@ -15,15 +15,20 @@ $(function(){
       var show_user;
       show_user = $('#show_user').data('show-user');
       show_creator = $('#messages-creator').data('creator-id');
+      show_creator1 = $('#messages-creator-1').data('creator-id');
+      show_user = $('#messages-user').data('user-id');
       console.log(show_creator);
+      console.log(show_creator1);
+      console.log(show_user);
       console.log(data['chat_user']);
       console.log(show_user);
-      if (data['chat_user'] === show_user) {
-        return $('#messages').append(data['message_right']);
+      if (data['chat_user'] === show_user || show_creator === show_creator1) {
+        $('.chat_box').animate({scrollTop : $('.chat_box')[0].scrollHeight });
+        $('.chat_box').append(data['message_right']);
       } else {
-        return $('#messages').append(data['message_left']);
+        $('.chat_box').animate({scrollTop : $('.chat_box')[0].scrollHeight });
+        $('.chat_box').append(data['message_left']);
       }
-      
     },
     speak: function(message) {
       return this.perform('speak', {
