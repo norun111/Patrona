9b6c2617b03f89c2bab2fdc20f0a0a5dd2781595
tomoyaueno29 <u.t01280729@gmail.com>
2020-05-05 23:54:11 +0900

    Update manu-bar.js

--- app/assets/javascripts/manu-bar.js
+++ app/assets/javascripts/manu-bar.js
@@ -1,12 +1,14 @@
-$(document).ready(function(){
-  
-  $(".menuItem").hover(function(){
-    var pos=$(this).attr("alt");
+$(document).on('turbolinks:load', function(){
+  $(document).ready(function(){
     
- $( "#slider" ).stop().animate({
-    marginLeft: 150*(pos-1),
-  }, 300)
+    $(".menuItem").hover(function(){
+      var pos=$(this).attr("alt");
       
-      
+  $( "#slider" ).stop().animate({
+      marginLeft: 150*(pos-1),
+    }, 300)
+        
+        
+    });
   });
 });
