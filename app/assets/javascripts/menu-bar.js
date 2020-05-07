cacb706332fe8f60f69d3fcd0754d4c4543cddc3
tomoyaueno29 <u.t01280729@gmail.com>
2020-05-07 14:24:10 +0900

    Create menu-bar.js

---
+++ app/assets/javascripts/menu-bar.js
@@ -0,0 +1,14 @@
+$(function(){
+  $(document).ready(function(){
+    
+    $(".menuItem").hover(function(){
+      var pos=$(this).attr("alt");
+      
+  $( "#slider" ).stop().animate({
+      marginLeft: 150*(pos-1),
+    }, 300)
+        
+        
+    });
+  });
+});
