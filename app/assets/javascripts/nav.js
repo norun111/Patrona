36b54c7b998fcd53695ead712513489b34b81558
tomoyaueno29 <u.t01280729@gmail.com>
2020-05-07 14:22:26 +0900

    Update nav.js

--- app/assets/javascripts/nav.js
+++ app/assets/javascripts/nav.js
@@ -1,10 +1,9 @@
-$(document).on('turbolinks:load', function(){
-
-  $(function() {
+$(function() {
     var $win = $(window),
       $cloneNav = $('#nav').clone().addClass('clone-nav').appendTo('body'),
       showClass = 'is-show';
       var nav = $("#nav");
+      nav.css('display','none');
     $win.on('load scroll', function() {
       var value = $(this).scrollTop();
       if ( value > 800 ) {
@@ -16,4 +15,3 @@ $(document).on('turbolinks:load', function(){
       }
     });
   });
-});
